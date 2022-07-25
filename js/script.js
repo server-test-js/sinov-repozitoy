1590bdec6a5d4c238c78f7332c52da1b4a95a00e
server-test-js <examo@gmail.com>
2022-07-25 12:52:04 -0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+"use strict"
+// let place = confirm("Siz Toshkentda yashaysizmi ?")
+
+// if (place) {
+//     alert("Saytdan foydalanishingiz mumkin")
+// }
+// else if (!place) {
+//     alert("Siz saytga kira olmaysiz")
+// }
+
+const number = +prompt("Yoshingiz nechada", "18")
+
+if (number === 18) {
+    console.log(18 + 20)
+}
